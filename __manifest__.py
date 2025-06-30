0465e438465185a3e8badca9346ceda50cf49c15
Muhammed Anas <muhammadanaswayanad@gmail.com>
2025-06-30 14:34:29 +0530

    Update CRM Stage Subcategory module: bump version to 17.0.1.9.0, improve visibility conditions for substage selection, and enhance wizard alert messaging.

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'CRM Stage Subcategory',
-    'version': '17.0.1.8.0',
+    'version': '17.0.1.9.0',
     'category': 'Sales/CRM',
     'summary': 'Add subcategories to CRM stages',
     'description': """
